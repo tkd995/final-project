e8ee77b77651347b674f96a85f24d81559ff9661
tkd995 <tkd995@vols.utk.edu>
2024-12-05 13:04:15 -0500

    Update httpserver.py

--- httpserver.py
+++ httpserver.py
@@ -4,24 +4,21 @@ import json
 import thread
 import socketserver
 
-#import tensorflow as tf
-
-
 class connhandler(socketserver.BaseRequestHandler):
-	context = []
+	context = []#active session context
 	#model = tf.keras.saving.load_model("model.keras")
 
 	def generate_output(self):
-		req = requests.post("http://localhost:11434/api/chat", data=json.dumps({"model":"llama3.2:1b", "messages":self.context, "stream":False}), stream=False, timeout=9999)
+		req = requests.post("http://localhost:11434/api/chat", data=json.dumps({"model":"llama3.2:1b", "messages":self.context, "stream":False}), stream=False, timeout=9999)#ollama request
 		try:
-			v = req.json()["message"]
+			v = req.json()["message"]#information about who generated the text and what text
 			self.context.append(v)
 			print(v["content"])
 		except KeyError:
 			print(str(req.text))
 
 	def handle(self):
-		i = self.request.recv(4098).decode()
+		i = self.request.recv(4098).decode()#on request with text, pull text from HTTP req, save for context and send to LLM, send reqult
 		i = i[i.rfind(":")+2:-2]
 		print(i)
 		if(i == "rs"):
@@ -32,5 +29,5 @@ class connhandler(socketserver.BaseRequestHandler):
 			self.request.send(self.context[-1]["content"].encode())
 		
 
-with socketserver.TCPServer(("", 80), connhandler) as httpd:
+with socketserver.TCPServer(("", 80), connhandler) as httpd:#run HTTP server permanently, socketserver so it can multithread
 	httpd.serve_forever()
